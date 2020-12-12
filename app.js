28f10ee5aa794c789d09fd1b48f3a1da5f7fb07e
moha1313128 <moha1313128@gmail.com>
2020-12-12 22:32:23 +0100

    Improved Development Workflow and Debugging

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-// core module: http - https - fs - path - os
-const http = require("http");
-const routes = require("./routes");
-
-// console.log(routes.someText);
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
+console.log("Debugging...");
