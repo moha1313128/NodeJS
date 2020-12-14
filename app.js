5e8351aed7c27dea3b0ce9c7b5fd8d6a3ee0597c
moha1313128 <moha1313128@gmail.com>
2020-12-14 18:59:06 +0100

    Express JS

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Debugging...");
+const express = require("express");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const path = require("path");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "404.html"));
+});
+app.listen(3000);
